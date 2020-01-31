6b1340054413caddf36c226ee24f15ab0613f4b3
Justin <jli208@hawk.iit.edu>
2020-01-30 23:14:24 -0600

    finalized graph upload

--- hw1/src/graph.py
+++ hw1/src/graph.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
-data = ['100', '10000', '1000000']
+data = [2, 4, 6] # log(100), log(10000), log(1000000)
 t1 = [0.15, 7.16, 721.991]
 t2 = [0.005, 0.016, 1.436]
 ax.plot(data, t1)
-ax.set(xlabel='data', ylabel='time (s)')
+ax.set(title='generate-dataset.sh time vs # of records plot', xlabel='Number of records[log(n)]', ylabel='time (s)')
 ax.grid()
 
 fig.savefig("generate.png")
@@ -13,8 +13,7 @@ plt.show()
 
 fig2, ax2 = plt.subplots()
 ax2.plot(data, t2)
-ax2.set(xlabel='data', ylabel='time (s)')
+ax2.set(title='sort-data.sh time vs # of records plot', xlabel='Number of records[log(n)]', ylabel='time (s)')
 ax2.grid()
-
-fig.savefig("sorting.png")
+fig2.savefig("sorting.png")
 plt.show()

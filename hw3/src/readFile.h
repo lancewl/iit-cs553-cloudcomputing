db5dbbf82fba5d65b6b331df0f12a4497cc059b3
Justin <jli208@hawk.iit.edu>
2020-03-29 18:04:52 -0500

    hw3: readFile upload. renamed namespace from workload to ReadTest

--- hw3/src/readFile.h
+++ hw3/src/readFile.h
@@ -17,32 +17,39 @@
 using namespace std;
 using namespace std::chrono;
 
-namespace workload{
-    int workload_test();
+namespace ReadTest{
+    void workload_init();
+    void workload_clean();
+
+    int rs_rr_test();
+    int write_rs_rr_results(const string &fileprefix, int start, int end);
+
+    int iops_rr_test();
+    int write_iops_rr_results(const string &rsize_prefix, int i_recordSize);
 
     void worker(int workerId, const string &filename, unsigned long recordSize, bool randFlag);
     
-    int d1_rs();
-    int d1_rr();
+    
 
-    int d2_rs();
-    int d2_rr();
+    int d1_rs(bool bigFileFlag, int i_recordSize);
+    int d1_rr(bool bigFileFlag, int i_recordSize);
 
-    int d3_rs();
-    int d3_rr();
+    int d2_rs(bool bigFileFlag, int i_recordSize);
+    int d2_rr(bool bigFileFlag, int i_recordSize);
 
-    int d4_rs();
-    int d4_rr();
+    int d3_rs(bool bigFileFlag, int i_recordSize);
+    int d3_rr(bool bigFileFlag, int i_recordSize);
 
-    int d5_rs();
-    int d5_rr();
+    int d4_rs(bool bigFileFlag, int i_recordSize);
+    int d4_rr(bool bigFileFlag, int i_recordSize);
 
-    int d6_rs();
-    int d6_rr();
+    int d5_rs(bool bigFileFlag, int i_recordSize);
+    int d5_rr(bool bigFileFlag, int i_recordSize);
 
-    int d7_rs();
-    int d7_rr();
+    int d6_rs(bool bigFileFlag, int i_recordSize);
+    int d6_rr(bool bigFileFlag, int i_recordSize);
 
-    int open_d1_rs();
+    int d7_rs(bool bigFileFlag, int i_recordSize);
+    int d7_rr(bool bigFileFlag, int i_recordSize);
 
 }
--- hw3/src/scripts/createfiles.sh
+++ hw3/src/scripts/createfiles.sh
@@ -5,6 +5,8 @@
 rm -rf data
 mkdir data
 cd data
+mkdir 10G
+cd 10G
 # D1
 mkdir D1; cd D1 
 dd if=/dev/zero of=0 bs=1GB count=10
@@ -64,5 +66,70 @@ for i in {0..47}
 do
     dd if=/dev/zero of=$i bs=$blocksize count=$n
 done
+cd ../
+echo "10G done"
+cd ../
 
-echo "done"
+
+# 1G
+mkdir 1G
+cd 1G
+# D1
+mkdir D1; cd D1 
+dd if=/dev/zero of=0 bs=1GB count=1
+cd ../
+
+# D2
+mkdir D2; cd D2
+dd if=/dev/zero of=0 bs=500MB count=1
+dd if=/dev/zero of=1 bs=500MB count=1
+cd ../
+
+# D3
+mkdir D3; cd D3
+blocksize=50MB
+n=5
+for i in {0..3}
+do
+    dd if=/dev/zero of=$i bs=$blocksize count=$n
+done
+cd ../
+
+# D4
+mkdir D4; cd D4
+blocksize=25MB
+n=5
+for i in {0..7}
+do
+    dd if=/dev/zero of=$i bs=$blocksize count=$n
+done
+cd ../
+
+# D5
+mkdir D5; cd D5
+blocksize=83MB
+n=1
+for i in {0..11}
+do
+    dd if=/dev/zero of=$i bs=$blocksize count=$n
+done
+cd ../
+
+# D6
+mkdir D6; cd D6
+blocksize=41MB
+n=1
+for i in {0..23}
+do
+    dd if=/dev/zero of=$i bs=$blocksize count=$n
+done
+cd ../
+
+# D7
+mkdir D7; cd D7
+blocksize=20MB
+n=1
+for i in {0..47}
+do
+    dd if=/dev/zero of=$i bs=$blocksize count=$n
+done

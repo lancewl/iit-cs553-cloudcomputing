b501277f1d618ca15bbfa28279ea544505f055e4
Kevin Tchouate Mouofo <ktchouatemouofo@hawk.iit.edu>
2020-03-31 16:03:04 +0000

    Test Iozone

--- hw3/src/Iozone_test.sh
+++ hw3/src/Iozone_test.sh
@@ -4,14 +4,14 @@ read workload
 echo "Enter the number of threads"
 read concur
 
-if [["$workload" -eq "RS"]]
+if [[ "$workload" == "RS" ]]
 then 
     workload=1
     for record in 64k 1m 16m
     do
         iozone -T -I -i 0 -i $workload -s $(((10*1024)/concur))m -t $concur -r $record
     done
-elif [["$workload" -eq "RR"]]
+elif [[ "$workload" == "RR" ]]
 then
     workload=2
     for record in 64k 1m 16m

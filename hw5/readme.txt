368fb98014f0d2b16a3b254972797c1e2021c11f
Justin <jli208@hawk.iit.edu>
2020-04-05 00:02:17 -0500

    hw5: updated readme and main.cpp

--- hw5/readme.txt
+++ hw5/readme.txt
@@ -1,1 +1,14 @@
 TODO: write me
+# makefile needed.
+
+# for now compile with
+~/team-25/hw5$ g++ src/* -o main
+
+# and run with
+~/team-25/hw5$ ./main [inputFileName] [memSize] [debugFlag(0/1/2/3)]
+
+e.g. ./main file.txt 8 0 // for normal run
+     ./main file.txt 8 1 // for lance's branch
+     ./main file.txt 8 2 // for kevin's branch
+
+# Add your test code in your specified if branch (main.cpp: line 29)
--- hw5/src/main.cpp
+++ hw5/src/main.cpp
@@ -2,20 +2,58 @@
 #include "merge.h"
 #include "io.h"
 
-#define AVAILABLE_MEM 16 // in GB
+#include <iostream>
+
+#define MAX_MEM 64 // in GB
+
+// run instructions
+// ./prog [gensort-filename] [memSize] [debugFlag(0/1/2/3)]
+
 
 int main(int argc, char *argv[]){
-    // cmd line arg parsing
-    std::string filename;
-    int memSize = AVAILABLE_MEM; // amount of memory availble in 
+    // cmd line arg parsing. Can add more user input sanitization if needed.
+    int numArgs = 3;
+    
+    if(argc != numArgs+1){
+        fprintf(stderr, "Incorrect args. Usage: %s [gensort-filename] [memSize]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    std::string filename = argv[1];
+    int debugFlag = atoi(argv[3]);
+    int memSize = atoi(argv[2]); // user specified memSize
+    if(memSize > MAX_MEM){ // mem specified too large
+        fprintf(stderr, "User specified %dGB of memory. Too large (MAX_MEM=%dGB)\n", memSize, MAX_MEM);
+        exit(EXIT_FAILURE);
+    }
 
+    if(debugFlag!=0){ // debug runs
+        std::cout << "DEBUG BRANCH " << debugFlag << std::endl;
+        if(debugFlag==1){
+            // debug branch for lance 
+            // test your code here
+            // ...
+
+        }else if(debugFlag==2){
+            // debug branch for kevin
+            // test your code here
+            // ...
+
+        }else if(debugFlag==3){
+            // debug branch for justin
+            
+        }else{
+            fprintf(stderr, "DEBUGFLAG ERROR\n"); exit(EXIT_FAILURE);
+        }
+        return 1;
+    }
+
+    // normal runs
     // check filesize using function in io.cpp, in namespace io.
 
     // if size is smaller than memSize, do simple in-memory sort
 
     // otherwise call function in merge.cpp to do external sort i.e. k-way merge
 
-
-    cout << "IMPLEMENTATION INCOMPLETE." << endl;
+    std::cout << "IMPLEMENTATION INCOMPLETE." << std::endl;
     return 0;    
 }

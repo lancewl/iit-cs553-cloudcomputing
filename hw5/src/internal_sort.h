1ed29cfcb362d31cf3c8f2924526d72b11e41ead
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2020-04-12 13:59:38 +0800

    hw5: delete unused merge sort in internal sort

--- hw5/src/internal_sort.h
+++ hw5/src/internal_sort.h
@@ -3,8 +3,6 @@
 
 #include <string>
 
-void mergeSort(std::string &s, int l, int r);
-void merge(std::string &s, int l, int m, int r);
 void heapSort(std::string strarr[], int n);
 void heapify(std::string strarr[], int n, int i);
 

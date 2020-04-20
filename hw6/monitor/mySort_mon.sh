5e6b9bd80b7eff087487de07b86647708cec9e33
Ubuntu <ubuntu@ubuntu.local>
2020-04-20 06:29:43 +0000

    hw6: update monitoring graph

--- hw6/monitor/mySort_mon.sh
+++ hw6/monitor/mySort_mon.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
  
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-    echo "Usage: mySort_mon.sh [dataset size in GB] [output file name]"
+    echo "Usage: mySort_mon.sh [dataset size in GB]"
     exit
+fi
 
 echo "Generating $1 GB dataset..."
 gensort -a $((10000000*$1)) gs.out
 
 echo "Sorting $1 GB dataset..."
-pidstat -rudh --human 1 -e ./mySort gs.out 8 0 > mySort_$1G_mon.log
+pidstat -rudh --human 1 -e ./mySort gs.out 8 > monitor/mySort_$1G_mon.log
 
+echo "Validating sorted data..."
+valsort sort.out
+
+echo "Plotting performance graph..."
+python3 monitor/plot.py monitor/mySort_$1G_mon.log monitor/mySort_$1G_mon.png
+
+echo "removing the datasets"
 rm gs.out sort.out
